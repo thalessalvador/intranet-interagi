19733feede59abda0c36edad5051bab279e14224
thalessalvador <thaless@gmail.com>
2023-05-12 19:27:47 -0300

    Novo Passo de Atualizacao

---
+++ backend/src/intranet_interagi/tests/content/test_pessoa.py
@@ -0,0 +1,55 @@
+from intranet_interagi.content.pessoa import Pessoa
+from plone import api
+from plone.dexterity.fti import DexterityFTI
+from zope.component import createObject
+
+import pytest
+
+
+CONTENT_TYPE = "Pessoa"
+
+
+@pytest.fixture
+def payload() -> dict:
+    """Payload to create a new Pessoa."""
+    return {
+        "type": CONTENT_TYPE,
+        "title": "Alex Limi",
+        "description": "Criador do Plone",
+        "email": "limi@plone.org",
+        "ramal": "1975",
+        "id": "alex-limi",
+    }
+
+
+class TestPessoa:
+    @pytest.fixture(autouse=True)
+    def _fti(self, get_fti, integration):
+        self.fti = get_fti(CONTENT_TYPE)
+
+    def test_fti(self):
+        assert isinstance(self.fti, DexterityFTI)
+
+    def test_factory(self):
+        factory = self.fti.factory
+        obj = createObject(factory)
+        assert obj is not None
+        assert isinstance(obj, Pessoa)
+
+    @pytest.mark.parametrize(
+        "behavior",
+        [
+            "plone.namefromtitle",
+            "plone.shortname",
+            "plone.excludefromnavigation",
+            "plone.versioning",
+        ],
+    )
+    def test_has_behavior(self, get_behaviors, behavior):
+        assert behavior in get_behaviors(CONTENT_TYPE)
+
+    def test_create(self, portal, payload):
+        with api.env.adopt_roles(["Manager"]):
+            content = api.content.create(container=portal, **payload)
+        assert content.portal_type == CONTENT_TYPE
+        assert isinstance(content, Pessoa)
