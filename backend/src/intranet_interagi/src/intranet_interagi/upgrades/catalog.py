1943acbc816036e67b2b49fd67e8aea261a26e1e
thalessalvador <thaless@gmail.com>
2023-05-23 13:38:45 -0300

    Exercicio 56

--- backend/src/intranet_interagi/src/intranet_interagi/upgrades/catalog.py
+++ backend/src/intranet_interagi/src/intranet_interagi/upgrades/catalog.py
@@ -10,3 +10,13 @@ def reindexa_pessoa(context):
         pessoa.reindexObject(idxs=["area"])
         logger.info(f"- Reindexa o campo area do objeto {pessoa.absolute_url()}")
     logger.info("Reindexação completa")
+
+
+def reindexa_predio(context):
+    # Reindexa o campo predio do tipo pessoa
+    brains = api.content.find(portal_type=["Area", "Pessoa"])
+    for brain in brains:
+        conteudo = brain.getObject()
+        conteudo.reindexObject(idxs=["predio"])
+        logger.info(f"- Reindexa o campo predio do objeto {conteudo.absolute_url()}")
+    logger.info("Reindexação completa")
