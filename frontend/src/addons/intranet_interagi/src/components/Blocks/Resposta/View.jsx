4cc2145109e62fe15b9c567be3e69f0b06b2d8e5
thalessalvador <thaless@gmail.com>
2023-05-30 19:42:59 -0300

    Exercicio até 73

---
+++ frontend/src/addons/intranet_interagi/src/components/Blocks/Resposta/View.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RespostaView = (props) => {
+  return (
+    <div className={'block blockResposta'}>
+      <div className={'container'}>
+        <h3 className={'value'}>42</h3>
+      </div>
+    </div>
+  );
+};
+
+export default RespostaView;
--- frontend/src/addons/intranet_interagi/src/index.js
+++ frontend/src/addons/intranet_interagi/src/index.js
@@ -1,5 +1,9 @@
+import RespostaView from './components/Blocks/Resposta/View';
 import AreaView from './components/View/AreaView';
 import PessoaView from './components/View/PessoaView';
+
+// Icones
+import exclamationSVG from '@plone/volto/icons/exclamation.svg';
 
 const applyConfig = (config) => {
   config.settings = {
@@ -14,6 +18,24 @@ const applyConfig = (config) => {
     Area: AreaView,
     Pessoa: PessoaView,
   };
+
+  config.blocks.blocksConfig.respostaBlock = {
+    id: 'respostaBlock',
+    title: 'Resposta',
+    group: 'intranet',
+    icon: exclamationSVG,
+    view: RespostaView,
+    restricted: false,
+    mostUsed: false,
+    sidebarTab: false,
+    blockHasOwnFocusManagement: false,
+  };
+
+  config.blocks.groupBlocksOrder = [
+    ...config.blocks.groupBlocksOrder,
+    { id: 'intranet', title: 'Intranet' },
+  ];
+
   return config;
 };
 
